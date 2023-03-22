93ba075752c4f5fe04b731a6d3e0afcc69565b29
KAJONGI <kajongi2323@gmail.com>
2023-03-21 18:16:58 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,21 @@
 #include "main.h"
+
 /**
- * print_alphabet - print all alphabet in lowercase
+ * print_alphabet - Entry point
+ * Return:0
  */
+
 void print_alphabet(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
 
